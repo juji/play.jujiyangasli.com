6096938dd2bf1e2de5ed3245ff2e56d01f9217d6
juji <jujiyangasli@gmail.com>
2019-07-13 08:30:33 +0700

    Updates

---
+++ precache-manifest.0d8c56a060922744f3e397412d0c8d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b9a1c68647566405b719ab6a0c0b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb87e67380bfb1d5786",
+    "url": "/static/css/main.9cf40b51.chunk.css"
+  },
+  {
+    "revision": "dbf8e2aa25df7140abbe",
+    "url": "/static/js/2.2d9f984d.chunk.js"
+  },
+  {
+    "revision": "bbb87e67380bfb1d5786",
+    "url": "/static/js/main.540c5e82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c22bd8170fbc75769f09041a072df9e7.js"
+  "/precache-manifest.0d8c56a060922744f3e397412d0c8d38.js"
 );
 
 self.addEventListener('message', (event) => {
